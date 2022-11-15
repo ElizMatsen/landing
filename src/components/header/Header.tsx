01bce940a74e274edba462bf1836e1b4f642f08f
liza_matsenka <macenko1996@gmail.com>
2022-11-16 00:01:02 +0300

    added  header layout + styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import classNames from "classnames";
+import logo from "../../assets/img/logo.svg";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
-
+    const [menu, setMenu] = React.useState(false);
+    const openMenu = () => {
+        setMenu(!menu)
+    }
     return (
-        <div>Header</div>
+        <>
+            <header>
+                <div className="container">
+                    <div className="header__container">
+                        <div className="header__logo">
+                            <img className="header__logo-image" src={logo} alt="Логотип"/>
+                        </div>
+                        <div className={classNames('menu', menu ? ' menu__open' : 'menu__close')}>
+                            <nav className="navigation">
+                                <ul className="navigation__container">
+                                    <li className="navigation__item" onClick={openMenu}>
+                                        <NavLink to="/business">
+                                            Бизнес
+                                        </NavLink>
+                                    </li>
+                                    <li className="navigation__item" onClick={openMenu}>
+                                        <NavLink to="/">
+                                            Цены
+                                        </NavLink>
+                                    </li>
+                                    <li className="navigation__item" onClick={openMenu}>
+                                        <NavLink to="/">
+                                            О нас
+                                        </NavLink>
+                                    </li>
+                                    <li className="navigation__item" onClick={openMenu}>
+                                        <NavLink to="/">
+                                            Оформить заказ
+                                        </NavLink>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div className={classNames('burger', menu ? ' burger__active' : '')} onClick={openMenu}>
+                <span className="burger__container">
+                </span>
+                </div>
+            </header>
+        </>
     )
 };
 
